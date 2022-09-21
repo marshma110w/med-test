8c5ac2a3e6e78eaea642dedea1422e0f72fc7fc0
Michael <m.chichunov@erlyvideo.org>
2022-09-21 21:08:02 +0300

    add-scaffold-groups-with-links-generation

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,79 @@
+class GroupsController < ApplicationController
+  before_action :set_group, only: %i[ show edit update destroy code ]
+
+  # GET /groups or /groups.json
+  def index
+    @groups = Group.all
+  end
+
+  # GET /groups/1 or /groups/1.json
+  def show
+  end
+
+  def to_quiz
+    cookies[:group_id] = params[:id]
+    redirect_to quiz_quiz_path
+  end
+
+  # GET /groups/new
+  def new
+    @group = Group.new
+  end
+
+  # GET /groups/1/edit
+  def edit
+  end
+
+  # POST /groups or /groups.json
+  def create
+    @group = Group.new(group_params)
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to groups_url, notice: "Группа успешно создана"  }
+        format.json { render :index, status: :created }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /groups/1 or /groups/1.json
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to groups_url, notice: "Группа успешно изменена"  }
+        format.json { render :index, status: :ok }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /groups/1 or /groups/1.json
+  def destroy
+    @group.destroy
+
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: "Группа успешно удалена" }
+      format.json { head :no_content }
+    end
+  end
+
+  def code
+    @svg = RQRCode::QRCode.new(code_group_url(@group)).as_svg
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_group
+      @group = Group.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def group_params
+      params.fetch(:group, {}).permit(:custom_name)
+    end
+end
