55cade0220088cd45699d57fda676744be4e4426
Michael <m.chichunov@erlyvideo.org>
2022-11-17 07:45:59 +0300

    models and controllers init

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,7 @@
+class AdminsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'sessions/login'
+  get 'welcome', to: 'sessions#welcome'
+  resources :admins, only: [:new, :create]
+
   resources :groups do
     member do
       get :code
       get :to_quiz
     end
   end
+
   get 'quiz/welcome'
   get 'quiz/final'
   get 'quiz/quiz'
