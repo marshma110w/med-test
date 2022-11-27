d70a29493b0f3c2199fe95db6f4ab83d8e545e55
Michael <m.chichunov@erlyvideo.org>
2022-11-27 23:54:27 +0300

    take-personal-info-before-test

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -2,6 +2,20 @@ class QuizController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :attempt
   skip_before_action :admin_auth_check, only: [:quiz, :final, :attempt]
 
+  def welcome
+  end
+
+  def personal_info
+    attempt = Attempt.new
+    attempt.name = params[:name]
+    attempt.phone = params[:phone]
+    attempt.email = params[:email]
+    attempt.group_id = params[:group_id]
+    attempt.save!
+    p attempt
+    redirect_to quiz_code_path(group_id: params[:group_id], attempt_id: attempt.id)
+  end
+
   def quiz
   end
 
@@ -10,9 +24,7 @@ class QuizController < ApplicationController
 
   def attempt
 
-    attempt = Attempt.new
-
-    attempt.group = Group.find(params[:group_id])
+    attempt = Attempt.find(params[:attemptId].to_i)
 
     attempt.asked_questions_count = params[:askedQuestionsCount]
 
